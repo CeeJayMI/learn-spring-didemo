aeff8da188824d0eacc44feecd309950c052d511
ceejaymi <jonathan@jonathanslaven.com>
2022-06-07 13:24:41 -0400

    Initial commit

---
+++ src/main/java/com/jonathanslaven/learnspringdidemo/LearnSpringDidemoApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanslaven.learnspringdidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringDidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringDidemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonathanslaven/learnspringdidemo/LearnSpringDidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanslaven.learnspringdidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringDidemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
